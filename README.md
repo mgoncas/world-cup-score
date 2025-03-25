5a97ff124813f21b9e47071708ce4bd0e72b9a28
mgoncas <mgcastro86@gmail.com>
2025-03-25 11:37:57 +0100

    Swagger Actuator Add ResponseEntity

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # world-cup-score
 assess your knowledge and skills
+
+
+# Swagger
+http://localhost:8080/swagger-ui/index.html
+
+# Actuator
+explicar
+- http://localhost:8080/actuator/health
+- http://localhost:8080/actuator/info
+- http://localhost:8080/actuator/metrics
+- ej: http://localhost:8080/actuator/metrics/http.server.requests
--- src/main/java/com/sportradar/worldcupscore/controller/BetController.java
+++ src/main/java/com/sportradar/worldcupscore/controller/BetController.java
@@ -3,6 +3,8 @@ package com.sportradar.worldcupscore.controller;
 import com.sportradar.worldcupscore.model.Bet;
 import com.sportradar.worldcupscore.service.BetProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,19 +18,19 @@ public class BetController {
     }
 
     @PostMapping("/bets")
-    public String addBet(@RequestBody Bet bet) {
+    public ResponseEntity<Bet> addBet(@RequestBody Bet bet) {
         betProcessor.addBet(bet);
-        return "Bet added successfully.";
+        return ResponseEntity.status(HttpStatus.CREATED).body(bet);
     }
 
     @GetMapping("/summary")
-    public String getSummary() {
-        return betProcessor.getSummary();
+    public ResponseEntity<String> getSummary() {
+        return ResponseEntity.ok(betProcessor.getSummary());
     }
 
     @PostMapping("/shutdown")
-    public String shutdownSystem() {
+    public ResponseEntity<String> shutdownSystem() {
         betProcessor.shutdownSystem();
-        return "System shutdown initiated.";
+        return ResponseEntity.ok("System shutdown initiated.");
     }
 }
